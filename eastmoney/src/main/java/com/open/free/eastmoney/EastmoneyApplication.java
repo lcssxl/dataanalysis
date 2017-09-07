50a0236a1e2012a3b6a1c5afa8fcbdb00f857eaa
Changsheng Liu <lcssxl@gmail.com>
2017-09-07 17:51:37 +0800

    数据库设计完善

---
+++ eastmoney/src/main/java/com/open/free/eastmoney/EastmoneyApplication.java
@@ -0,0 +1,15 @@
+package com.open.free.eastmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lcssx on 9/7/2017.
+ */
+@SpringBootApplication
+public class EastmoneyApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(EastmoneyApplication.class, args);
+    }
+}
